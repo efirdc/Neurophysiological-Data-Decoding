f023a2c5187d747736392eb2aef0f455197521f0
Cory Efird <cefird101@gmail.com>
2021-09-08 07:18:34 -0600

    Experimenting with BigGAN sampling.

---
+++ research/scripts/biggan_sample.py
@@ -0,0 +1,34 @@
+import torch
+from pytorch_pretrained_biggan import (BigGAN, one_hot_from_names, truncated_noise_sample,
+                                       save_as_images, display_in_terminal)
+
+# OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
+import logging
+logging.basicConfig(level=logging.INFO)
+
+# Load pre-trained model tokenizer (vocabulary)
+model = BigGAN.from_pretrained('biggan-deep-256')
+
+# Prepare a input
+truncation = 0.4
+class_vector = one_hot_from_names(['dog', 'cat', "pizza"], batch_size=3)
+noise_vector = truncated_noise_sample(truncation=truncation, batch_size=3)
+
+# All in tensors
+noise_vector = torch.from_numpy(noise_vector)
+class_vector = torch.from_numpy(class_vector)
+
+# If you have a GPU, put everything on cuda
+noise_vector = noise_vector.to('cuda')
+class_vector = class_vector.to('cuda')
+model.to('cuda')
+
+# Generate an image
+with torch.no_grad():
+    output = model(noise_vector, class_vector, truncation)
+
+# If you have a GPU put back on CPU
+output = output.to('cpu')
+
+# Save results as png images
+save_as_images(output)
